cd7a4539e835af507abb47b33970ca4c4a36463a
Loïc Dornel <creeperstone72.redstoner@gmail.com>
2023-03-16 16:15:24 +0100

    view: Page home

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    icon: 'DDS',
+    title: 'DDS | Station Service des Alpes',
+  });
 });
 
 module.exports = router;
