a20a2d3bb5eadca56675a55e0c1faa1fad3fd02c
Loïc Dornel <creeperstone72.redstoner@gmail.com>
2023-03-24 11:14:26 +0100

    Front

--- python/main.py
+++ python/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, send_from_directory
 from flask_restful import Api, Resource
 
 
@@ -7,22 +7,39 @@ api = Api(app)
 
 print('hi')
 
+
 class Input(Resource):
     def post(self):
         data = request.get_json()
         return jsonify(data)
 
+
 api.add_resource(Input, '/input')
 
-# first page for payment
+
+# Images
+@app.route('/images/<path:path>')
+def images(path):
+    return send_from_directory('static/img', path)
+
+
+# Main page
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Page for payment
+@app.route('/payment')
 def payment():
     return render_template('payment.html')
 
-# second page for pump
+
+# Page for pump
 @app.route('/pump')
 def pump():
     return render_template('pump.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
