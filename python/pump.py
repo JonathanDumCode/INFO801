750a00397f0f06c507bc56fd7c5b9804be648abe
TamarSH99 <80041512+TamarSH99@users.noreply.github.com>
2023-03-27 16:31:52 +0200

    add 4 class

---
+++ python/pump.py
@@ -0,0 +1,11 @@
+import code as code
+import vehicle as vehicle
+
+
+class pump():
+    def __init__(self, dispensedFuels):
+        self.dispensedFuels = dispensedFuels
+
+
+    def pumpGas(self, code, vehicle):
+        pass
